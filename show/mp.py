dcc05a517f808e2ee6016d1394a66e85564e828a
TheusMarqs <matheusmarqsilva@gmail.com>
2023-09-25 10:10:13 -0300

    tentando implementar api mercado pago

--- show/mp.py
+++ show/mp.py
@@ -1,1 +1,17 @@
 import requests
+
+
+url = 'https://api.mercadopago.com'
+
+#Chave de autenticação
+payload = {}
+headers = {
+    'Authorization': 'TEST-67bae85d-2d4d-4843-8483-beb6dc42385a'
+}
+
+#Recuperando as informações
+response = requests.request("GET", url, headers=headers, data=payload)
+
+#Bruto recuperado em string
+print(response.text)
+a = response.text.split(',')
--- show/views.py
+++ show/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Show
 from django.urls import reverse_lazy
 from .forms import ShowForm
-from django.utils import timezone
-import datetime
+import mercadopago
+from core import settings
 
 def exibir_shows(request):
     shows = Show.objects.all()
@@ -32,6 +32,33 @@ def ingresso(request, id):
     shows = Show.objects.filter(id=id)
     for show in shows:
         artist = show.artist
+    
+
+    sdk = mercadopago.SDK("TEST-6540474833878444-092507-74467a0e593855cfde4d00bb2d3b0c4b-1218207994")
+
+
+    payment_data = {
+        "transaction_amount": float(request.POST.get("transaction_amount")),
+        "token": request.POST.get("token"),
+        "description": request.POST.get("description"),
+        "installments": int(request.POST.get("installments")),
+        "payment_method_id": request.POST.get("payment_method_id"),
+        "notification_url": "http://requestbin.fullcontact.com/1ogudgk1",
+        "payer": {
+            "email": request.POST.get("email"),
+            "identification": {
+                "type": request.POST.get("type"), 
+                "number": request.POST.get("number")
+            }
+        }
+    }
+
+    payment_response = sdk.payment().create(payment_data)
+    payment = payment_response["response"]
+
+
+    print(payment)
+
     return render(request, 'ingresso.html', {'shows': shows, 'artist': artist})
 
 def filtrar_shows(request, city):
